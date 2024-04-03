e57a152e4b8a5a87894c5df6e58f1c133d2febd2
dx7 <dx7@protonmail.ch>
2024-04-03 13:32:01 -0300

    improve majority element

--- 169-majority-element/majority-element.rb
+++ 169-majority-element/majority-element.rb
@@ -1,17 +1,19 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def majority_element(nums)
-  nums.sort!
-  count = 1
-  candidate = nums[0]
+  count = 0
+  candidate = 0
   i = 0
 
-  while ((i + 1) < nums.size) && (count <= (nums.size / 2.0))
-    if nums[i] == nums[i+1]
-      count += 1
+  while i < nums.size
+    if count == 0
+      candidate = nums[i]
+    end
+
+    if nums[i] == candidate
+      count += 1;
     else
-      candidate = nums[i+1]
-      count = 1
+      count -= 1;
     end
 
     i += 1
