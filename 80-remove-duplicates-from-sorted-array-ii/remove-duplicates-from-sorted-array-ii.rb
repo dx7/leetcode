d722113cf0a9b356a9e1f340ef538dd7384ab641
dx7 <dx7@protonmail.ch>
2024-04-08 19:27:15 -0300

    update remove duplicates from sorted array ii

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.rb
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.rb
@@ -1,27 +1,43 @@
 # @param {Integer[]} nums
 # @return {Integer}
+# def remove_duplicates(nums)
+#   i = 0
+#   j = 1
+#   first_duplicate = false
+#
+#   while j < nums.size
+#     if nums[j] > nums[i] || !first_duplicate
+#       if nums[j] > nums[i]
+#         first_duplicate = false
+#       elsif not first_duplicate
+#         first_duplicate = true
+#       end
+#
+#       if j != i + 1
+#         nums[i + 1] = nums[j]
+#       end
+#
+#       i += 1
+#     end
+#
+#     j += 1
+#   end
+#
+#   i + 1
+# end
+
 def remove_duplicates(nums)
-  i = 0
+  i = 1
   j = 1
-  first_duplicate = false
 
   while j < nums.size
-    if nums[j] > nums[i] || !first_duplicate
-      if nums[j] > nums[i]
-        first_duplicate = false
-      elsif not first_duplicate
-        first_duplicate = true
-      end
-
-      if j != i + 1
-        nums[i + 1] = nums[j]
-      end
-
+    if i == 1 || nums[j] > nums[i-2]
+      nums[i] = nums[j]
       i += 1
     end
 
     j += 1
   end
 
-  i + 1
+  i
 end
