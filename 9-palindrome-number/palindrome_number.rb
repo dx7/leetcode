2a8464a466a61dbf4a80dbd0d9dea1899fc612f2
dx7 <dx7@protonmail.ch>
2024-04-29 15:19:56 -0300

    update palindrome number

--- 9-palindrome-number/palindrome_number.rb
+++ 9-palindrome-number/palindrome_number.rb
@@ -1,14 +1,15 @@
 # @param {Integer} x
 # @return {Boolean}
 def is_palindrome(x)
-  word = x.to_s
-  palindrome_check_position(word, 0, word.size - 1)
-end
+  return false if x.negative?
 
-def palindrome_check_position(word, i_begin, i_end)
-  if i_begin >= i_end
-    true
-  else
-    (word[i_begin] == word[i_end]) && palindrome_check_position(word, i_begin + 1, i_end - 1)
+  n = x
+  reverse = 0
+
+  while n.positive?
+    reverse = reverse * 10 + n % 10
+    n /= 10
   end
+
+  x == reverse
 end
